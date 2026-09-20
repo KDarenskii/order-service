53dad95733ff7f0fb68b3aa6a7178ed6e43038a9
Legenda <legenda@gmail.com>
2026-09-20 22:30:13 +0300

    TASK-4002: добавил логирование ошибок в sentry

--- internal/app/config/section/monitor.go
+++ internal/app/config/section/monitor.go
@@ -3,4 +3,10 @@ package section
 type Monitor struct {
 	LogLevel    string `split_words:"true" default:"debug"`
 	Environment string `default:"development"`
+	Sentry      MonitorSentry
+}
+
+type MonitorSentry struct {
+	Enabled bool   `default:"false"`
+	DSN     string `default:""`
 }
